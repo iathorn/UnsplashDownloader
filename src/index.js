e270ae47ef598d212ba8bda3a7cc0d1a5c1a8aa6
Dongho Choi <evals4dead@gmail.com>
2019-03-14 13:06:13 +0900

    exec.sh

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import Koa from "koa";
 import Router from "koa-router";
 import bodyParser from "koa-bodyparser";
 import dotenv from "dotenv";
+import axios from "axios";
+import curlirize from "axios-curlirize";
 import api from "./api";
 
 dotenv.config();
@@ -9,6 +11,8 @@ dotenv.config();
 const { PORT: port = 4000 } = process.env;
 
 const app = new Koa();
+
+curlirize(axios);
 
 const router = new Router();
 router.use("/api", api.routes());
@@ -18,4 +22,8 @@ app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(port, () => {
   console.log("port:", port);
+  axios
+    .get("http://localhost:4000/api/unsplash")
+    .then(res => console.log("hello", res))
+    .catch(err => console.log(err));
 });
